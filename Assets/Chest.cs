3ca611dba6e66ff697bebdd1b3380734ed749caf
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-05-31 19:41:29 +0200

    Dodanie interakcji postaci ze skrzynką

---
+++ Assets/Chest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{ 
+   
+  [Header("Chest")]
+  [SerializeField] private GameObject closeChest;
+  [SerializeField] private GameObject openChest;
+  [SerializeField] private Material chestMaterial;
+  [SerializeField] private GameObject decisonPanel;
+  
+  private bool _nearChest;
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.tag=="Player")
+      {
+         _nearChest = true;
+      }
+   }
+
+   private void OnMouseEnter()
+   {
+      chestMaterial.color = Color.gray;
+   }
+
+   private void OnMouseExit()
+   {
+      chestMaterial.color = Color.white;
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (other.gameObject.tag=="Player")
+      {
+         _nearChest = false;
+      }
+   }
+
+   private void OnMouseDown()
+   {
+      if (_nearChest)
+      {
+         openChest.SetActive(true);
+         closeChest.SetActive(false);
+      }
+   }
+   
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,34 +7,42 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("to fill")]
     [SerializeField] private CharacterController controller;
-    [SerializeField] private Transform camera;
+    [SerializeField] private Transform parentCam;
     [Header("Parameters")]
     [SerializeField] private float speed=6;
-    [SerializeField] private float turnSmooth = 0.1f;
-
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    
     private float _turnSmoothVelocity;
-
-    private void Start()
+    void Update()
     {
-        Cursor.visible = false;
-        Cursor.lockState= CursorLockMode.Locked;
+        Movement();
+        RotationCam();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Movement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal,0,vertical).normalized;
         if (direction.magnitude >=0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg+ camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, turnSmooth);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0,angle,0);
-
-
+            
             Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+    private void RotationCam()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            parentCam.rotation = Quaternion.Euler(0,parentCam.eulerAngles.y-speed,0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            parentCam.rotation = Quaternion.Euler(0,parentCam.eulerAngles.y+speed,0);
+        } 
     }
 }
